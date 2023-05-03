de1d205d9ff409643a2ef84369847f39a8bca6cd
robdatta <ssdatta@ncsu.edu>
2023-05-02 21:47:14 -0400

    new program added to main

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
-int main(int, char **)
+/*
+    Undertsanding cerr,clog and cout
+*/
+
+void understanding_cerr_clog_cout()
 {
-    cout<<endl;
-    cout << "Hello, word,I am GOD of warss !\n";
+    int a=10,b=0,c;
+    if(b==0)
+    {
+        cerr << "This is error:Divide by zero error" << endl;
+        b=20;
+    }
+    c=a/b;
+    cout << "This is cout:Value of c is " << c << endl;
+    clog << "This is clog:Value of c is " << c << endl;
+}
+
+int main()
+{
+    understanding_cerr_clog_cout();
+    return 0;
 }
